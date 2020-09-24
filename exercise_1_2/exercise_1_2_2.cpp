4f21bbbc393e44a3788953abbfdcb27ec125cb81
Pranathi Singareddy <prsingareddy@gmail.com>
2020-09-24 01:38:40 +0000

    Completed exercise 1.2.2

--- exercise_1_2/exercise_1_2_2.cpp
+++ exercise_1_2/exercise_1_2_2.cpp
@@ -9,8 +9,29 @@ int main()
     std::cout << "Enter String:" << std::endl;
     std::cin >> str;
 
+
     // Palindrome Check
     // WRITE YOUR PALINDROME CHECK HERE
+
+    bool is_palindrome = true;
+    int start = 0;
+    int end = str.size() - 1;
+
+
+    while (is_palindrome == true && (start < end)) {
+    	if (str[start] == str[end]) {
+    		start++;
+    		end--;
+    	} else {
+    		is_palindrome = false;
+    	}
+    }
+
+    if (is_palindrome == true) {
+    	std::cout << str << " is a palindrome." << std::endl;
+    } else {
+    	std::cout << str << " is not a palindrome." << std::endl;
+    }
 
     return 0;
 }
